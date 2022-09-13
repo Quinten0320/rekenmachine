e7ad1cdd5ab8e1a4b9c480fda6f71afb0175a924
Quinten Wit <s1182530@student.windesheim.nl>
2022-09-13 14:14:53 +0200

    made multiplication and divison

--- rekenmachine.cpp
+++ rekenmachine.cpp
@@ -5,9 +5,6 @@ int main(void) {
 	int b;
 	int c;
 	int d;
-	int e;
-	int f;
-	int g;
 
 	printf("What do you want to do? (type the number)\n 1. calculator\n");
 	scanf_s("%d", &a);
@@ -19,14 +16,26 @@ int main(void) {
 		if (b == 2)
 		{
 			printf("type 2 numbers (seperated by space)\n");
-			scanf_s("%d %d", &f, &g);
-			printf("%d - %d = %d", f, g, f - g);
+			scanf_s("%d %d", &c, &d);
+			printf("%d - %d = %d", c, d, c - d);
 		}
 		else if (b == 1)
 		{
 			printf("type 2 numbers (seperated by space)\n");
 			scanf_s("%d %d", &c, &d);
 			printf("%d + %d = %d", c, d, c + d);
+		}
+		else if (b == 3)
+		{
+			printf("type 2 numbers (seperated by space)\n");
+			scanf_s("%d %d", &c, &d);
+			printf("%d * %d = %d", c, d, c * d);
+		}
+		else if (b == 4)
+		{
+			printf("type 2 numbers (seperated by space)\n");
+			scanf_s("%d %d", &c, &d);
+			printf("%d / %d = %d", c, d, c / d);
 		}
 	}
 	return 0;
